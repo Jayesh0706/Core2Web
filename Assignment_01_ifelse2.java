fbabe19ccc7b800830d6e8754f347941aebe596e
Jayesh Nalawade <92593054+Jayesh0706@users.noreply.github.com>
2023-10-01 21:06:39 +0530

    Add files via upload

---
+++ Assignment_01_ifelse2.java
@@ -0,0 +1,15 @@
+public class Assignment_01_ifelse2 {
+    public static void main(String[] args) {
+//        int var=5;
+//        int var=16;
+        int var=10;
+        if(var>10){
+            System.out.println(var+" is Greater than 10");
+        } else if (var<10)
+        {
+            System.out.println(var +" is Less than 10");
+        }else {
+            System.out.println("Equal");
+        }
+    }
+}
